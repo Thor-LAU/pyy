29168e4c78ce4496816c174ed568fc007f60026d
Thor-LAU <ThorLau990104@gmail.com>
2021-08-26 17:12:52 +0800

    A new project

---
+++ src/main/java/com/pyyexclusivenew/pyy/PyyApplication.java
@@ -0,0 +1,13 @@
+package com.pyyexclusivenew.pyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PyyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pyyexclusivenew/pyy/PyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyyexclusivenew.pyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
